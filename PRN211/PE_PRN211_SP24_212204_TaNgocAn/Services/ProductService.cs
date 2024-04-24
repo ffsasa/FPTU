97d2c64971884e8bbb0a8e34954962ebea8a4baa
ffsasa <ngocan2003krp@gmail.com>
2024-04-24 08:37:09 +0700

    Temp

---
+++ PRN211/PE_PRN211_SP24_212204_TaNgocAn/Services/ProductService.cs
@@ -0,0 +1,35 @@
+﻿using Repositories;
+using Repositories.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class ProductService
+    {
+        private ProductRepository repository = new ProductRepository();
+
+        public List<Product> GetProducts()
+        {
+            return repository.GetProducts();
+        }
+
+        public void AddProduct(Product product)
+        {
+            repository.AddProduct(product);
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            repository.UpdateProduct(product);
+        }
+
+        public void DeleteProduct(Product product)
+        {
+            repository.DeleteProduct(product);
+        }
+    }
+}
---
+++ PRN211/PRN211 Chuẩn bị/Chuẩn bị/BookManagement_An/Login.cs
@@ -0,0 +1,40 @@
+﻿using BookManagement_An;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BookManagement_TaNgocAn
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSignIn_Click(object sender, EventArgs e)
+        {
+  
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Do you want to Quit?", "Quit", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
