5d12ca4209ec27446ed9e391c8e559d8bf2214cb
ffsasa <ngocan2003krp@gmail.com>
2025-02-07 19:12:09 +0700

    Update PRM

--- SEM_8/PRM392/Lab4/app/src/main/java/com/example/myapplication/DrinksActivity.java
+++ SEM_8/PRM392/Lab4/app/src/main/java/com/example/myapplication/DrinksActivity.java
@@ -1,4 +1,19 @@
 package com.example.myapplication;
 
-public class DrinksActivity {
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DrinksActivity extends AppCompatActivity {
+    private String selectedDrinks;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_drinks);
+
+
+    }
 }
--- SEM_8/PRM392/Lab4/app/src/main/java/com/example/myapplication/MainActivity.java
+++ SEM_8/PRM392/Lab4/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.myapplication;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String selectedFood = "";
+    private String selectedDrinks = "";
+    Button btnFood, btnDrinks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +26,74 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        btnDrinks = findViewById(R.id.btnDrinks);
+        btnFood = findViewById(R.id.btnFood);
+
+        ActivityResultLauncher<Intent> foodActivityLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        if (data != null) {
+                            selectedFood = data.getStringExtra("SELECTED_FOOD");
+                            // Xử lý kết quả từ FoodActivity
+                            updateUI();
+                        }
+                    }
+                }
+            }
+        );
+
+        ActivityResultLauncher<Intent> drinksActivityLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == RESULT_OK){
+                        Intent data = result.getData();
+                        if (data != null){
+                            selectedDrinks = data.getStringExtra("SELECTED_DRINKS");
+                            // Xử lý kết quả từ DrinksActivity
+                            updateUI();
+                        }
+                    }
+                }
+            }
+        );
+
+        btnFood.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, FoodActivity.class);
+                foodActivityLauncher.launch(intent);
+            }
+        });
+
+        btnDrinks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DrinksActivity.class);
+                drinksActivityLauncher.launch(intent);
+            }
+        });
+    }
+
+    public void updateUI(){
+        TextView txtResult = findViewById(R.id.txtResult);
+
+        if (!selectedFood.isEmpty() && !selectedDrinks.isEmpty()) {
+            txtResult.setText("Món đã chọn: " + selectedFood + " - " + selectedDrinks);
+        }
+        else if (!selectedFood.isEmpty()) {
+            txtResult.setText("Món đã chọn: " + selectedFood);
+        }
+        else if (!selectedDrinks.isEmpty()) {
+            txtResult.setText("Nước đã chọn: " + selectedDrinks);
+        }
+        else {
+            txtResult.setText("Chưa chọn món hoặc nước!");
+        }
     }
 }
