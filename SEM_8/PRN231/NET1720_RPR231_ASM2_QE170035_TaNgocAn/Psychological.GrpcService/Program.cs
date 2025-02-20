a206418093d5313b56052a815847cb2a8a99767c
ffsasa <ngocan2003krp@gmail.com>
2025-02-20 15:40:14 +0700

    PRN Update

---
+++ SEM_8/PRN231/NET1720_RPR231_ASM2_QE170035_TaNgocAn/Psychological.GrpcService/Program.cs
@@ -0,0 +1,25 @@
+﻿using Psychological.GrpcService.Services;
+
+namespace Psychological.GrpcService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddGrpc();
+            builder.Services.AddSingleton<SurveyServiceImpl>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            app.MapGrpcService<SurveyServiceImpl>();  // Đăng ký SurveyServiceImpl
+            app.MapGrpcService<GreeterService>();
+            app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+            app.Run();
+        }
+    }
+}
---
+++ SEM_8/PRN231/NET1720_RPR231_ASM2_QE170035_TaNgocAn/Psychological.GrpcService/Services/SurveyServiceImpl.cs
@@ -0,0 +1,36 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Psychological.GrpcService.Models;
+
+namespace Psychological.GrpcService.Services
+{
+    public class SurveyServiceImpl : SurveyService.SurveyServiceBase
+    {
+        private readonly SurveyService _surveyService;
+
+        // Constructor nhận vào SurveyService để gọi logic lấy dữ liệu
+        public SurveyServiceImpl(SurveyService surveyService)
+        {
+            _surveyService = surveyService;
+        }
+
+        // Triển khai phương thức GetAll
+        public override Task<SurveyList> GetAll(Empty request, ServerCallContext context)
+        {
+            var surveys = _surveyService.GetAll();  // Gọi phương thức từ SurveyService để lấy dữ liệu
+            var response = new SurveyList();
+
+            foreach (var survey in surveys)
+            {
+                response.Surveys.Add(new Survey
+                {
+                    Id = survey.Id,
+                    Description = survey.Description,
+                    // Các trường khác của Survey bạn có thể thêm ở đây
+                });
+            }
+
+            return Task.FromResult(response);
+        }
+    }
+}
