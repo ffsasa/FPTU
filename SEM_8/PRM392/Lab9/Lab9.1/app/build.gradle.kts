0bd0dd9368cd00d7db88ea11ff7a18f73d636f10
ffsasa <ngocan2003krp@gmail.com>
2025-02-20 16:18:41 +0700

    PRM Lab9.1

--- SEM_8/PRM392/Lab9/Lab9.1/app/build.gradle.kts
+++ SEM_8/PRM392/Lab9/Lab9.1/app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.lab91"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.lab91"
--- SEM_8/PRM392/Lab9/Lab9.1/app/src/main/java/com/example/lab91/MainActivity.java
+++ SEM_8/PRM392/Lab9/Lab9.1/app/src/main/java/com/example/lab91/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.lab91;
 
+import android.database.Cursor;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +12,25 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Database database;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        //Tao database GhiChu
+        database = new Database(this, "GhiChu.sqlite", null, 1);
+        // Tao table CongViec
+        database.QueryData("Create table if not exists CongViec(id Integer Primary Key Autoincrement," +
+                "TenCV nvarchar(200))");
+        // Insert data
+//         database.QueryData("Insert into CongViec values (null, 'Project Android')");
+//         database.QueryData("Insert into CongViec values(null, 'Design app')");
+        //Select data
+        Cursor dataCongViec = database.GetData("Select * from CongViec");
+        while (dataCongViec.moveToNext()) {
+            String ten = dataCongViec.getString(1);
+            Toast.makeText(this, ten, Toast.LENGTH_SHORT).show();
+        }
     }
 }
