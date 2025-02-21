b6d8b12d0ede7a3caf68ae6bf181d46179dd9d1c
ffsasa <ngocan2003krp@gmail.com>
2025-02-21 11:42:06 +0700

    Update PRN

---
+++ SEM_8/PRN231/NET1720_RPR231_ASM2_QE170035_TaNgocAn/Psychological.GrpcClient.ConsoleApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using Grpc.Net.Client;
+using Psychological.GrpcClient.ConsoleApp;
+using Psychological.GrpcService.Protos;
+
+namespace Psychological.GrpcClient.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Welcome to gRPC Client!");
+
+            using var channel = GrpcChannel.ForAddress("https://localhost:7270");
+            var client = new SurveyService.SurveyServiceClient(channel);
+
+            Console.WriteLine("Get All Survey");
+            var surveys = client.GetAll(new Empty());
+            if (surveys != null && surveys.Surveys.Count > 0)
+            {
+                foreach (var survey in surveys.Surveys)
+                {
+                    Console.WriteLine(string.Format("{0} - {1}", survey.Id, survey.Description));
+                }
+            }
+            else
+            {
+                Console.WriteLine("No survey found");
+            }
+            Console.WriteLine("\r\nRPCClient.GetById{Id={1}):");
+            var sủveyById = client.GetById(new SurveyIdRequest { Id = 1 });
+            Console.WriteLine(string.Format("{0} - {1}", sủveyById.Id, sủveyById.Description));
+
+
+
+        }
+    }
+}
--- SEM_8/PRN231/NET1720_RPR231_ASM2_QE170035_TaNgocAn/Psychological.GrpcService/Services/SurveyServiceImpl.cs
+++ SEM_8/PRN231/NET1720_RPR231_ASM2_QE170035_TaNgocAn/Psychological.GrpcService/Services/SurveyServiceImpl.cs
@@ -1,36 +1,69 @@
 ﻿using Google.Protobuf.WellKnownTypes;
 using Grpc.Core;
 using Psychological.GrpcService.Models;
+using Psychological.GrpcService.Protos;
 
 namespace Psychological.GrpcService.Services
 {
     public class SurveyServiceImpl : SurveyService.SurveyServiceBase
     {
-        private readonly SurveyService _surveyService;
-
-        // Constructor nhận vào SurveyService để gọi logic lấy dữ liệu
-        public SurveyServiceImpl(SurveyService surveyService)
+        private readonly ILogger<SurveyServiceImpl> _logger;
+        private static List<Protos.Survey> _surveys = new List<Protos.Survey>
         {
-            _surveyService = surveyService;
+            new Protos.Survey {
+            Id = 1,
+            CategoryId = 1,
+            Description = "Kiem tra 1",
+            Number = 1,
+            PointAverage = 1,
+            VeryGood = 1,
+            Good = 1,
+            Medium = 1,
+            Bad = 1,
+            VeryBad = 1,
+            CreateBy = 1,
+            CreateAt = DateTime.Now.ToString(),
+            UpdateAt = DateTime.Now.ToString()
+            },
+            new Protos.Survey() {
+            Id = 2,
+            CategoryId = 1,
+            Description = "Kiem tra 2",
+            Number = 1,
+            PointAverage = 1,
+            VeryGood = 1,
+            Good = 1,
+            Medium = 1,
+            Bad = 1,
+            VeryBad = 1,
+            CreateBy = 1,
+            CreateAt = DateTime.Now.ToString(),
+            UpdateAt = DateTime.Now.ToString()
+            }
+        };
+            
+
+        public SurveyServiceImpl(ILogger<SurveyServiceImpl> logger)
+        {
+            _logger = logger;
         }
 
-        // Triển khai phương thức GetAll
-        public override Task<SurveyList> GetAll(Empty request, ServerCallContext context)
+        public override async Task<SurveyList> GetAll(Protos.Empty request, ServerCallContext context)
         {
-            var surveys = _surveyService.GetAll();  // Gọi phương thức từ SurveyService để lấy dữ liệu
             var response = new SurveyList();
+            response.Surveys.AddRange(_surveys); 
 
-            foreach (var survey in surveys)
+            return await Task.FromResult(response); 
+        }
+
+        public override Task<Protos.Survey> GetById(SurveyIdRequest request, ServerCallContext context)
+        {
+            var survey = _surveys.FirstOrDefault(s => s.Id == request.Id);
+            if (survey == null)
             {
-                response.Surveys.Add(new Survey
-                {
-                    Id = survey.Id,
-                    Description = survey.Description,
-                    // Các trường khác của Survey bạn có thể thêm ở đây
-                });
+                throw new RpcException(new Status(StatusCode.NotFound, "Survey not found"));
             }
-
-            return Task.FromResult(response);
+            return Task.FromResult(survey);
         }
     }
 }
